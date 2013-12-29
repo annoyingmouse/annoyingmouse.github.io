e52e16cbf4ab2f9dd53e0450d1c72d68b3e86113
Dominic Myers <annoyingmouse@gmail.com>
2013-12-29 13:28:56 +0000

    name to id on internal links

--- js/templates.js
+++ js/templates.js
@@ -2,7 +2,7 @@ $.templates({
     addPost: '\
         <article data-id="{{>id}}" class="post">\
             <header>\
-                <h2><a name="{{>id}}">{{>title}}</a></h2>\
+                <h2><a id="{{>id}}">{{>title}}</a></h2>\
                 <p>Published: <time datetime="{{>pubdate}}">{{>date}}</time></p>\
             </header>\
             {{:content}}\
