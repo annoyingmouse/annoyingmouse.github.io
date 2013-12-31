b5e514da5d26ce8583440c0731cf55ccd4fffe0e
Dominic Myers <annoyingmouse@gmail.com>
2013-12-31 18:04:44 +0000

    Comments

--- js/templates.js
+++ js/templates.js
@@ -14,18 +14,18 @@ $.templates({
                 <form action="https://docs.google.com/forms/d/1p5O_oT5nhZeR2o6M5G2WH1a150W8eucRql_dH338rF8/formResponse" method="POST" target="hidden_iframe" onsubmit="submitted=true;" class="form-horizontal" data-sortable="{{>id}}">\
                     <fieldset>\
                         <legend>Your Comment</legend>\
-                        <input type="hidden" name="entry.1732417772" value="{{>id}}"/>\
+                        <input type="hidden" name="entry.645503591" value="{{>id}}"/>\
                         <div class="control-group">\
-                            <label class="control-label" for="entry_1188980396{{>id}}">Email Address</label>\
+                            <label class="control-label" for="entry_1915033572{{>id}}">Email Address</label>\
                             <div class="controls">\
-                                <input id="entry_1188980396{{>id}}" name="entry.1188980396" placeholder="email" class="input-xlarge span8" type="email" required>\
+                                <input id="entry_1915033572{{>id}}" name="entry.1915033572" placeholder="email" class="input-xlarge span8" type="email" required>\
                                 <p class="help-block">Please provide an email address associated with a <a href="http://gravatar.com">Gravatar</a></p>\
                             </div>\
                         </div>\
                         <div class="control-group">\
-                            <label class="control-label" for="entry_144231699{{>id}}">Comment</label>\
+                            <label class="control-label" for="entry_2006478066{{>id}}">Comment</label>\
                             <div class="controls">\
-                                <textarea rows="4" class="span8" id="entry_144231699{{>id}}" name="entry.144231699" required></textarea>\
+                                <textarea rows="4" class="span8" id="entry_2006478066{{>id}}" name="entry.2006478066" required></textarea>\
                             </div>\
                         </div>\
                         <div class="captcha_wrap">\
