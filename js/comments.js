f1eb7d4d558562384d1b2387dce27c84057da6fb
Dominic Myers <annoyingmouse@gmail.com>
2025-04-04 15:23:56 +0100

    🔧 (gitignore): add .idea directory to .gitignore file 📝 (workspace.xml): update workspace.xml to include new files and changes 📝 (index.html): update index.html to include new script sources ♻️ (comments.js, functions.js): refactor code in comments.js and functions.js for better readability and maintainability

--- js/comments.js
+++ js/comments.js
@@ -1,18 +1,25 @@
+function validateEmail(email) {
+  var re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
 $(function () {
   $("#content").on("submit", "form", function (e) {
     e.preventDefault();
-    // var theForm = $(e.target);
-    // var formId = theForm.attr("data-sortable");
-    // if ($('ul.sortable[data-id="' + formId + '"]').validate()) {
-    // 	if (validateEmail(theForm.find('input[type="email"]').val())) {
-    // 		theForm.find("textarea").val(theForm.find("textarea").val());
-    // 	} else {
-    // 		e.preventDefault();
-    // 		alert("Please Provide a valid email address!");
-    // 	}
-    // } else {
-    // 	e.preventDefault();
-    // 	alert("No, you are not a human!");
-    // }
+    debugger;
+    const theForm = $(e.target);
+    theForm.validate();
+    if (theForm.valid()) {
+      if (validateEmail(theForm.find('input[type="email"]').val())) {
+        theForm.find("textarea").val(theForm.find("textarea").val());
+      } else {
+        e.preventDefault();
+        alert("Please Provide a valid email address!");
+      }
+    } else {
+      e.preventDefault();
+      alert("No, you are not a human!");
+    }
   });
 });
